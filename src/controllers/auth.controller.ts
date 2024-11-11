5889d03d414e73011e072c49aaf165c534406ce8
javidnex <javid@nex.az>
2024-11-11 17:50:11 +0400

    Added prisma client to structure

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import type { cSignupRequestType } from '../types/types'
 import Validate from '../helpers/validate'
-import { PrismaClient } from '@prisma/client'
+import prisma from '../database/prisma'
 import jwt from 'jsonwebtoken'
 import hash from '../helpers/hash'
 
@@ -16,7 +16,7 @@ export const cSignup = async (request: FastifyRequest<{Body: cSignupRequestType}
     
     if(new Validate({must: 'email'}).condition(email) && password.length >= 8 && password.length <= 32)
     {
-        const prisma = new PrismaClient()
+        
         const user = await prisma.user.findUnique({ where: { email } })
 
         if(user)
